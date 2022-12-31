d85da55367926fcfb55a6d91e37c87dce0664f3a
unknown <dongjooyoung@naver.com>
2022-12-31 20:13:32 +0900

    querystring, body-json 데이터 주고 받기

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// 쿼리스트링으로 데이터 주고받기
 app.get('/querystring', (req, res) => {
-    res.send("쿼리스트링");
+    const {id} = req.query
+    res.send({id});
 });
 
 app.post('/body-formdata', (req, res) => {
@@ -15,10 +19,14 @@ app.post('/body-multipart', (req, res) => {
     res.send("body-multipart");
 });
 
+//body에 json형태로 데이터를 삽입하여 보내줌
 app.post('/body-json', (req, res) => {
-    res.send("body-json");
+    //객체구조분해할당으로 여러가지 데이터를 한 문장으로 작성하여 받아올 수 있음
+    const {id, password} = req.body
+    res.json({id,password})
 });
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 열렸어요!');
 });
+
